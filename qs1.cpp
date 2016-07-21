0ab513540bc935c997c71265aff27b7161094a1f
Sid <siddhata.s.patil@gmail.com>
2016-07-21 15:37:29 -0700

    Create qs1.cpp

---
+++ qs1.cpp
@@ -0,0 +1,13 @@
+/*
+ ==============================================================================================================================
+ Name        : qs1.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Write a recursive algorithm to compute the power function pow(x, y) = x^y
+ ==============================================================================================================================
+ */
+ 
+#include <iostream>
+using namespace std;
+
